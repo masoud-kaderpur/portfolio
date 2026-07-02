39437cbf8c2f1511c94122a1483fcb79742b682e
Masoud Kaderpur <e12539176@student.tuwien.ac.at>
2026-07-02 07:52:54 +0200

    first draft. modified homepage, footer and header

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+    output: "server",
+    adapter: cloudflare(),
+	integrations: [mdx(), sitemap()]
 });
